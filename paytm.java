03d7848ad9fc93ac7739f7a0d00d61a50fd8cb45
tom <tom@example.com>
2018-07-14 13:52:54 +0000

    Update the location tracker to instructors level

---
+++ paytm.java
@@ -0,0 +1,10 @@
+this is some new change by tom for paytm functionality
+Adding some new class for some purpose
+By Tom
+
+modifications by jerry
+Adding some content
+
+Fixing the problem to do the patching
+
+Adding a new function which will detect the driver details when notification come from driver app
